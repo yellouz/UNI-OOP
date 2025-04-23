810d08843e9a711ea07e0340572eba6830de487a
yellouz <youssefellouz303@gmail.com>
2025-04-23 11:20:00 +0100

    upload question 2

--- TP1/Fonctions avancées et mémoire dynamique en C++/EX2.cpp
+++ TP1/Fonctions avancées et mémoire dynamique en C++/EX2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 template <typename T> T maxValeur(T a, T b)
@@ -9,9 +10,36 @@ template <typename T> T maxValeur(T a, T b)
         return b;
 }
 
+template <typename T> T maxValeur(vector<T> &v)
+{
+    T maxVal = 0;
+    for (T &val : v)
+    {
+        if (maxVal < val)
+            maxVal = val;
+    }
+
+    return maxVal;
+}
+
 int main()
 {
-    float a = 12, b = 10;
+    int a = 12, b = 10;
+    cout << "La plus grande valeur entre int a et int b est: " << maxValeur(a, b) << endl;
 
-    cout << "La plus grande valeur est: " << maxValeur(a, b) << endl;
+    float c = 24.22, d = 23.87;
+    cout << "La plus grande valeur entre float c et float d est: " << maxValeur(c, d) << endl;
+
+    vector <int> intVector;
+    intVector.push_back(10);
+    intVector.push_back(20);
+    intVector.push_back(30);
+    cout << "La plus grande valeur dans intVector est: " << maxValeur(intVector) << endl;
+
+    vector <double> doubleVector;
+    doubleVector.push_back(19.99);
+    doubleVector.push_back(30);
+    doubleVector.push_back(30.01);
+    cout << "La plus grande valeur dans doubleVector est: " << maxValeur(doubleVector) << endl;
+
 }
